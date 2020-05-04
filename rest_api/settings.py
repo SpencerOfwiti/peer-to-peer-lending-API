ca943a99c086d4e16aa6679c1dce7dface1e740a
Spencer Ofwiti <maxspencer56@gmail.com>
2020-05-04 17:14:50 +0300

    Update settings.py
    Remove the secret key.

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm=y__4iy-3o2de6wehckm11_3_6be43!rav9*bfes%f)o)hi7o'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
